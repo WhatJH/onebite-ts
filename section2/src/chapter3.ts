fb37a8c7d7ac131da4ce1152b7c08d1024d54b54
WhatJH <030226jihyun@naver.com>
2025-08-30 23:49:11 +0900

    객체

---
+++ section2/src/chapter3.ts
@@ -0,0 +1,26 @@
+// object
+let user: {
+    id?: number;
+    name: string;
+} = {
+    id: 1,
+    name: "이정환",
+};
+
+// let dog: {
+//     name: string;
+//     color: string;
+// } = {
+//     name: "돌돌이",
+//     color: "brown",
+// };
+
+// user.id;
+
+let config: {
+    readonly apiKey: string;
+} = {
+    apiKey: "MY API KEY",
+};
+
+// config.apiKey = "hacked"
