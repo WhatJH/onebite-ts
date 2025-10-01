233a3137d4688cf492dcccd77e542ddcb6cd6806
WhatJH <030226jihyun@naver.com>
2025-10-01 18:41:04 +0900

    상태관리와 Props 1

--- section_11/src/App.tsx
+++ section_11/src/App.tsx
@@ -1,8 +1,36 @@
+import React, { useState,useRef, useEffect } from 'react';
 import './App.css';
+import Editor from './component/Editor';
+
+interface  Todo {
+  id: number;
+  content: string;
+}
 
 function App() {
-  return <div className="App"></div>
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const idRef = useRef(0);
+
+  const onClickAdd = (text: string) => {
+    setTodos([
+      ...todos,
+      {
+        id: idRef.current++,
+        content: text,
+      },
+    ]);
+  };
+
+  useEffect(() => {
+    console.log(todos);
+  }, [todos]);
   
-}
+  return( <div className="App">
+    <h1>Todo</h1>
+    <Editor onClickAdd={onClickAdd}/>
+  </div>
+  );
+};
 
 export default App;
